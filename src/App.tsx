aa18b00fc545a31aabef485d8af67b3ba57d4e3d
Gianluca L <gianluca@FGBAL062224.finconsgroup.local>
2025-01-08 15:56:46 +0100

    Added a topbar in mobile version

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import ScrollToTop from "react-scroll-to-top";
 import "./App.css";
 import { Content } from "./components/Content";
 import { Sidebar } from "./components/Sidebar";
+import { MobileTopbar } from "./components/MobileTopbar";
 
 const sideBarMinWidth = 150;
 const sideBarMaxWidth = 180;
@@ -9,15 +10,31 @@ const sideBarMaxWidth = 180;
 function App() {
   return (
     <div className="App">
-      <button
-        type="button"
-        className="btn d-sm-none"
-        style={{ position: "fixed", left: 15, top: 15, fontSize: "2rem" }}
+      <div
+        className="d-sm-none"
+        style={{
+          minHeight: 60,
+          width: "100%",
+          backgroundColor: "#292c34",
+          position: "fixed",
+          top: 0,
+          left: 0,
+          fontSize: "2rem",
+          zIndex: 666,
+          color: "whitesmoke",
+        }}
         data-bs-toggle="offcanvas"
         data-bs-target="#sidebar"
       >
-        <i className="bi bi-list"></i>
-      </button>
+        <button
+          type="button"
+          className="btn d-sm-none"
+          style={{ position: "fixed", fontSize: "2rem", color: "whitesmoke" }}
+        >
+          <i className="bi bi-list"></i>
+        </button>{" "}
+        <MobileTopbar />
+      </div>
       <div className="container-fluid">
         <div className="row">
           <div className="col-2 p-0 sidebar">
---
+++ src/components/MobileTopbar/index.tsx
@@ -0,0 +1,16 @@
+export const MobileTopbar = () => {
+  return (
+    <div
+      style={{
+        position: "fixed",
+        top: 4,
+        left: 55,
+        whiteSpace: "nowrap",
+        maxWidth: "100%",
+      }}
+    >
+      <span style={{ fontSize: "1.4rem" }}>Gianluca Lingua | </span>
+      <span style={{ fontSize: "0.8rem" }}>Frontend developer</span>
+    </div>
+  );
+};
